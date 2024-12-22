9da6f3cc0c91253a3b99cdcbaa33b86a86d49211
Afeef K K <45afeef@gmail.com>
2024-12-22 08:14:50 +0530

    documented and commented the file well using copilot

--- lib/src/features/assessment/p/controllers/assessment_controller.dart
+++ lib/src/features/assessment/p/controllers/assessment_controller.dart
@@ -8,70 +8,82 @@ import '../../do/assessment.dart';
 import '../../do/assessment_item.dart';
 import '../../do/repositories/assessment_repository.dart';
 
+/// Controller for managing assessments in the application.
+/// This class handles fetching assessments, managing current question state,
+/// and tracking time spent on each question.
 class AssessmentController extends GetxController {
+  /// Singleton instance of AssessmentController
   static AssessmentController get call => Get.find();
 
-  // Initialize AssessmentRepository - which is responsible to comunicate with data sources
+  /// Repository for accessing assessment data
   final AssessmentRepository _repo = Get.find<AssessmentRepository>();
 
-  /// AssessmentResult class will handle all the data storing related tasks in cache or memory
-  /// This will collect and update the user input and time taken by each question, in
-  /// AssessmentItemResponse object
+  /// Holds the result of the assessment, including user inputs and time taken per question
   Rx<AssessmentResult> assessmentResult = AssessmentResult().obs;
+
+  /// Holds the current assessment being taken
   Rx<Assessment> assessment = Assessment.empty().obs;
 
-  /// Stores the current question index. this helps in managing the state and context
+  /// Tracks the index of the current question being displayed
   Rx<int> currentQuestionIndex = (-1).obs;
 
+  /// Timer for tracking time spent on each question
   Timer? _timer;
 
-  // Update the current question index and start time
+  /// Updates the current question index and starts the timer
   set currentQuestion(int index) => currentQuestionIndex.value = index;
 
+  /// Checks if the exam is currently ongoing
   bool get isExamOngoing =>
       currentQuestionIndex.value >= 0 &&
       currentQuestionIndex.value < assessment.value.items.length;
 
+  /// Checks if the exam has started
   bool get isExamStarted => currentQuestionIndex.value != -1;
 
+  /// Gets the time spent on the current question in a readable format
   String get timeSpentOnCurrentQuestion =>
       _getTimeSpentOnQuestion(currentQuestionIndex.value);
 
+  /// Fetches all available assessments from the repository
   Future<List<Assessment>> fetchAllAssessments() async {
     return await _repo.getAllAssessments();
   }
 
+  /// Fetches a specific assessment from the repository
   Future<Assessment> fetchAssessment() async {
     Assessment response = await _repo.getAssessment(1);
-
     assessment.value = response;
-
     return response;
   }
 
+  /// Gets the student's selection for a given assessment item
   String? getSelectionFor(AssessmentItem item) {
     final index = assessment.value.items.indexOf(item);
-
     return assessmentResult.value.studentResponse[index]?.studentAnswer;
   }
 
+  /// Handles the student's response to a question
   void handleStudentResponse(dynamic response, BuildContext context) {
     assessmentResult.value.studentResponse[currentQuestionIndex.value]!
         .updateResponse(response.toString());
 
+    // Display a snackbar with the selected answer
     Get.closeAllSnackbars();
     Get.snackbar('Selected Answer is', response.toString());
 
-    assessmentResult.refresh(); // Notify listeners about the change
+    // Notify listeners about the change in assessment result
+    assessmentResult.refresh();
   }
 
   @override
   void onClose() {
+    // Stop the exam and clean up resources
     stopExam();
-
     super.onClose();
   }
 
+  /// Starts the exam and initializes the timer
   void startExam() {
     if (assessment.value == Assessment.empty()) return;
 
@@ -82,46 +94,43 @@ class AssessmentController extends GetxController {
     _timer = Timer.periodic(1.seconds, (_) => _updateElapsedTime());
   }
 
+  /// Stops the exam and resets variables
   void stopExam() {
-    _timer?.cancel(); // Cancel the timer to prevent memory leaks
+    // Cancel the timer to prevent memory leaks
+    _timer?.cancel();
 
-    // reset all variables
+    // Reset all variables
     assessmentResult = AssessmentResult().obs;
     currentQuestionIndex.value = -1;
   }
 
+  /// Returns the total time spent on a specific question in a readable format
   String _getTimeSpentOnQuestion(int questionIndex) {
-    // Return the total time spent on a specific question
-
     int seconds = assessmentResult
             .value.studentResponse[questionIndex]?.timeTakenInMillisecond ??
         0;
-
     return seconds.toReadableTimeDelta(pattern: 'm:s');
   }
 
-  /// IMPROVEMENT APPRECIATED.
-  /// WHY ?
-  /// why we use [timeSpent] as 1?
-  /// the private method [_updateElapsedTime] is invoked by the timer with an interval of 1 second
-  /// so the extra time spent on current question will always be 1 second more than the previous
+  /// Updates the elapsed time for the current question
+  /// The private method [_updateElapsedTime] is invoked by the timer with an interval of 1 second
+  /// so the extra time spent on the current question will always be 1 second more than the previous
   ///
-  /// We are not minimizing the interval, because we don't want to compramize performance.
-  /// Hope this don't affect the quality of assessmentItem level student analytics
+  /// We are not minimizing the interval, because we don't want to compromise performance.
+  /// Current method of timer will miscount the time spent because we are using only one timer and
+  /// not resetting it for each question.
   ///
-  /// IGNORED
-  /// Current method of timer will miscount the time spent. because we are using only one timer and
-  /// not reseting it for each questions.
-  /// Hope this don't affect the quality of assessmentItem level student analytics
+  /// This approach aims to balance performance and accuracy.
   void _updateElapsedTime() {
     // Calculate the time spent on the current question
-    var timeSpent = 1;
+    var timeSpent = 1; // Each interval is 1 second
 
     // Update the total time for the current question
     if (assessmentResult.value.studentResponse
         .containsKey(currentQuestionIndex.value)) {
       assessmentResult.value.studentResponse[currentQuestionIndex.value]!
-          .incrementTimeTaken(timeSpent * 1000);
+          .incrementTimeTaken(
+              timeSpent * 1000); // Increment time in milliseconds
     } else {
       AssessmentItemResponse response =
           AssessmentItemResponse(timeTakenInMillisecond: timeSpent * 1000);
--- lib/src/features/homepage/p/creation/creationpage.dart
+++ lib/src/features/homepage/p/creation/creationpage.dart
@@ -18,6 +18,37 @@ class CreationPage extends GetWidget<CreationController> {
     );
   }
 
+  Widget _buildCreationIntentionPage() {
+    return SafeArea(
+      child: ListView(
+        padding: const EdgeInsets.all(16),
+        children: [
+          RoundedImageWithButton(
+            "${'create'.tr} ${'exam'.tr}",
+            'https://cdn.dribbble.com/userupload/8649959/file/original-9c6c904f1a4c04b2ecf4eff7e1a22e4f.png?resize=752x&vertical=center',
+            onPressed: () {},
+          ),
+          RoundedImageWithButton(
+            "${'create'.tr} ${'shorts'.tr}",
+            'https://th.bing.com/th/id/OIP.FS6YltTUtgofukeesFx21AHaHa?w=610&h=610&rs=1&pid=ImgDetMain',
+            onPressed: () => controller.isCreating = true,
+          ),
+          RoundedImageWithButton(
+            "${'create'.tr} ${'batch'.tr}",
+            'assets/images/school_batches.png',
+            isAssetImage: true,
+            onPressed: null,
+          ),
+          RoundedImageWithButton(
+            "${'create'.tr} ${'video_class'.tr}",
+            'https://th.bing.com/th/id/OIP.obLEgRLmdz9aDxstTuWd5AHaE7?rs=1&pid=ImgDetMain',
+            onPressed: null,
+          ),
+        ],
+      ),
+    );
+  }
+
   Widget _buildCreationPage() {
     return ShortsCreationWidget(
       onSubmit: (title, description, videoUrl, videoSource) {
@@ -25,37 +56,6 @@ class CreationPage extends GetWidget<CreationController> {
             title, description, videoUrl, videoSource);
         controller.submitCreation();
       },
-    );
-  }
-
-  Widget _buildCreationIntentionPage() {
-    return SafeArea(
-      child: ListView(
-        padding: const EdgeInsets.all(16),
-        children: [
-          RoundedImageWithButton(
-            'Create Exam',
-            'https://cdn.dribbble.com/userupload/8649959/file/original-9c6c904f1a4c04b2ecf4eff7e1a22e4f.png?resize=752x&vertical=center',
-            onPressed: () {},
-          ),
-          RoundedImageWithButton(
-            'Create Shorts',
-            'https://th.bing.com/th/id/OIP.FS6YltTUtgofukeesFx21AHaHa?w=610&h=610&rs=1&pid=ImgDetMain',
-            onPressed: () => controller.isCreating = true,
-          ),
-          const RoundedImageWithButton(
-            'Create New Batch',
-            'assets/images/school_batches.png',
-            isAssetImage: true,
-            onPressed: null,
-          ),
-          const RoundedImageWithButton(
-            'Create Video Class',
-            'https://th.bing.com/th/id/OIP.obLEgRLmdz9aDxstTuWd5AHaE7?rs=1&pid=ImgDetMain',
-            onPressed: null,
-          ),
-        ],
-      ),
     );
   }
 }
