7415de4f3d1c481f8edbf6782f7737004e77e4e7
Afeef K K <45afeef@gmail.com>
2025-01-03 23:20:13 +0530

    Updated the google login for web framework

--- lib/src/features/authentication/auth_service.dart
+++ lib/src/features/authentication/auth_service.dart
@@ -1,4 +1,7 @@
+import 'dart:io' show Platform;
+
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:get/get.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
@@ -17,20 +20,40 @@ class AuthService {
   get onLoggingIn => _loggingState;
 
   void signIn() {
-    _loggingState.value = true;
-    signInWithGoogle().then((_) {
-      _loggingState.value = false;
-      Get.offAllNamed(AppRoute.home);
-    }).catchError((onError) {
-      _loggingState.value = false;
-    });
+    if (kIsWeb) {
+      print('Running on the web');
+      signInWithGoogleInWeb().then((_) {
+        _loggingState.value = false;
+        Get.offAllNamed(AppRoute.home);
+      }).catchError((onError) {
+        _loggingState.value = false;
+      });
+    } else if (Platform.isAndroid) {
+      print('Running on Android');
+
+      _loggingState.value = true;
+      signInWithGoogleInAndroid().then((_) {
+        _loggingState.value = false;
+        Get.offAllNamed(AppRoute.home);
+      }).catchError((onError) {
+        _loggingState.value = false;
+      });
+    } else if (Platform.isIOS) {
+      print('Running on iOS');
+    } else if (Platform.isMacOS) {
+      print('Running on macOS');
+    } else if (Platform.isWindows) {
+      print('Running on Windows');
+    } else if (Platform.isLinux) {
+      print('Running on Linux');
+    } else {
+      print('Running on an unknown platform');
+    }
   }
 
-  Future<UserCredential> signInWithGoogle() async {
+  Future<UserCredential> signInWithGoogleInAndroid() async {
     // Trigger the authentication flow
-    final GoogleSignInAccount? googleUser = await GoogleSignIn(
-            clientId: const String.fromEnvironment('GOOGLE_CLIENT_ID'))
-        .signIn();
+    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
     // Obtain the auth details from the request
     final GoogleSignInAuthentication? googleAuth =
         await googleUser?.authentication;
@@ -43,6 +66,21 @@ class AuthService {
 
     // Once signed in, return the UserCredential
     return await _firebaseAuth.signInWithCredential(credential);
+  }
+
+  Future<UserCredential> signInWithGoogleInWeb() async {
+    // Create a new provider
+    GoogleAuthProvider googleProvider = GoogleAuthProvider();
+
+    googleProvider
+        .addScope('https://www.googleapis.com/auth/contacts.readonly');
+    googleProvider.setCustomParameters({'login_hint': 'user@example.com'});
+
+    // Once signed in, return the UserCredential
+    return await FirebaseAuth.instance.signInWithPopup(googleProvider);
+
+    // Or use signInWithRedirect
+    // return await FirebaseAuth.instance.signInWithRedirect(googleProvider);
   }
 
   void signOut() {
