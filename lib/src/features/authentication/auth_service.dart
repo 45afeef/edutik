162a8ff57f7019c356ed57410fad35845057992a
Afeef K K <45afeef@gmail.com>
2025-01-12 22:50:41 +0530

    converted AuthService to singleton

--- lib/src/features/authentication/auth_service.dart
+++ lib/src/features/authentication/auth_service.dart
@@ -8,12 +8,23 @@ import 'package:google_sign_in/google_sign_in.dart';
 import '../../../utils/routes.dart';
 
 class AuthService {
+  // Singleton instance
+  static final AuthService _instance = AuthService._privateConstructor();
+
   final RxBool _loggingState = false.obs;
 
   final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
 
   bool isAuthenticated =
       FirebaseAuth.instance.currentUser?.isAnonymous == false;
+
+  // Factory constructor to return the singleton instance
+  factory AuthService() {
+    return _instance;
+  }
+
+  // Make this a private constructor
+  AuthService._privateConstructor();
 
   User? get currentUser => _firebaseAuth.currentUser;
 
@@ -22,7 +33,7 @@ class AuthService {
   void signIn() {
     if (kIsWeb) {
       print('Running on the web');
-      signInWithGoogleInWeb().then((_) {
+      _signInWithGoogleInWeb().then((_) {
         _loggingState.value = false;
         Get.offAllNamed(AppRoute.home);
       }).catchError((onError) {
@@ -30,9 +41,8 @@ class AuthService {
       });
     } else if (Platform.isAndroid) {
       print('Running on Android');
-
       _loggingState.value = true;
-      signInWithGoogleInAndroid().then((_) {
+      _signInWithGoogleInAndroid().then((_) {
         _loggingState.value = false;
         Get.offAllNamed(AppRoute.home);
       }).catchError((onError) {
@@ -51,42 +61,37 @@ class AuthService {
     }
   }
 
-  Future<UserCredential> signInWithGoogleInAndroid() async {
+  void signOut() {
+    FirebaseAuth.instance.signOut().then((_) {
+      // TODO - This is not working well or not a good practice. Please make use of global stream listener to handle user auth state changes and navigation.
+      Get.offAllNamed(AppRoute.home);
+    });
+  }
+
+  Future<UserCredential> _signInWithGoogleInAndroid() async {
     // Trigger the authentication flow
     final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
     // Obtain the auth details from the request
     final GoogleSignInAuthentication? googleAuth =
         await googleUser?.authentication;
-
     // Create a new credential
     final credential = GoogleAuthProvider.credential(
       accessToken: googleAuth?.accessToken,
       idToken: googleAuth?.idToken,
     );
-
     // Once signed in, return the UserCredential
     return await _firebaseAuth.signInWithCredential(credential);
   }
 
-  Future<UserCredential> signInWithGoogleInWeb() async {
+  Future<UserCredential> _signInWithGoogleInWeb() async {
     // Create a new provider
     GoogleAuthProvider googleProvider = GoogleAuthProvider();
-
     googleProvider
         .addScope('https://www.googleapis.com/auth/contacts.readonly');
     googleProvider.setCustomParameters({'login_hint': 'user@example.com'});
-
     // Once signed in, return the UserCredential
     return await FirebaseAuth.instance.signInWithPopup(googleProvider);
-
     // Or use signInWithRedirect
     // return await FirebaseAuth.instance.signInWithRedirect(googleProvider);
-  }
-
-  void signOut() {
-    FirebaseAuth.instance.signOut().then((_) {
-      // TODO - This is not working well or not a good practice. Please make use of global stream listner to handle user auth state changes and navigation.
-      Get.offAllNamed(AppRoute.home);
-    });
   }
 }
--- lib/utils/shared_prefs.dart
+++ lib/utils/shared_prefs.dart
@@ -1,26 +1,30 @@
 import 'package:shared_preferences/shared_preferences.dart';
 
 class SharedPrefsUtil {
+  // Singleton instance
   static final SharedPrefsUtil _instance = SharedPrefsUtil._internal();
+
   late SharedPreferences _prefs;
 
+  // Factory constructor to return the singleton instance
   factory SharedPrefsUtil() {
     return _instance;
   }
 
+  // Make this a private constructor
   SharedPrefsUtil._internal();
 
   Future<void> init() async {
     _prefs = await SharedPreferences.getInstance();
   }
 
+  bool isOnboardingShown() {
+    return _prefs.getBool('onboarding_shown') ?? false;
+  }
+
   /// Handle the data based on onboarding is shown or not.
   static Future<void> setOnboardingShown(bool shown) async {
     final prefs = await SharedPreferences.getInstance();
     prefs.setBool('onboarding_shown', shown);
-  }
-
-  bool isOnboardingShown() {
-    return _prefs.getBool('onboarding_shown') ?? false;
   }
 }
