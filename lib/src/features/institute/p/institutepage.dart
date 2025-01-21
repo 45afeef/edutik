9507216fa2d810f2b91f5a74e4611349fa2bd4fa
Afeef K K <45afeef@gmail.com>
2025-01-21 22:54:59 +0530

    Updated institute page

--- lib/src/features/institute/p/institutepage.dart
+++ lib/src/features/institute/p/institutepage.dart
@@ -1,9 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-import '../../../../utils/routing/approute.dart';
+import '../../homepage/p/w/upcomming_feature.dart';
 import '../do/entity/institute.dart';
 import 'controllers/institute_controller.dart';
+
+class AssessmentTab extends StatelessWidget {
+  final List<String>? assessmentRefs;
+  const AssessmentTab(this.assessmentRefs, {super.key});
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: assessmentRefs?.length,
+      itemBuilder: (BuildContext context, int index) {
+        final assessmentId = assessmentRefs?[index];
+        return Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Card(
+            child: ListTile(
+              leading: Text('${index + 1}'),
+              title: const Text('Public Assessment'),
+              onTap: () {
+                // Navigate to the assessment page
+                Navigator.pushNamed(
+                  context,
+                  '/assessmentPage/$assessmentId',
+                );
+              },
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
 
 class Institutepage extends GetWidget<InstituteController> {
   const Institutepage({super.key});
@@ -30,55 +60,61 @@ class Institutepage extends GetWidget<InstituteController> {
                 }
 
                 Institute instituteData = snapshot.data!;
-                return Column(
-                  children: [
-                    Image.network(
-                      instituteData.profileUrl,
-                      fit: BoxFit.cover,
-                    ),
-                    const SizedBox(height: 20),
-                    Text(
-                      instituteData.name,
-                      textAlign: TextAlign.center,
-                      style: const TextStyle(
-                          fontSize: 24, fontWeight: FontWeight.bold),
-                    ),
-                    const SizedBox(height: 30),
-                    Text(
-                      instituteData.address,
-                      textAlign: TextAlign.center,
-                    ),
-                    const SizedBox(height: 20),
-                    Expanded(
-                      child: ListView.builder(
-                        shrinkWrap: true,
-                        itemBuilder: (BuildContext context, int index) {
-                          final assessmentId =
-                              instituteData.publicAssessmentRefs![index];
-                          return Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Card(
-                              child: AspectRatio(
-                                aspectRatio: 3.0,
-                                child: Center(
-                                  child: ListTile(
-                                    leading: Text('${index + 1}'),
-                                    title: Text(
-                                        '${'public'.tr} ${'assessment'.tr}'),
-                                    onTap: () => Get.toNamed(
-                                      AppRoute.assessmentPage
-                                          .replaceFirst(':id', assessmentId),
-                                    ),
-                                  ),
-                                ),
-                              ),
-                            ),
-                          );
-                        },
-                        itemCount: instituteData.publicAssessmentRefs!.length,
+                return NestedScrollView(
+                  headerSliverBuilder:
+                      (BuildContext context, bool innerBoxIsScrolled) {
+                    return [
+                      SliverAppBar(
+                        expandedHeight: 200.0,
+                        floating: false,
+                        flexibleSpace: FlexibleSpaceBar(
+                          background: Image.network(
+                            instituteData.profileUrl,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
                       ),
-                    )
-                  ],
+                      SliverList(
+                        delegate: SliverChildListDelegate([
+                          const SizedBox(height: 20),
+                          Text(
+                            instituteData.name,
+                            textAlign: TextAlign.center,
+                            style: const TextStyle(
+                                fontSize: 24, fontWeight: FontWeight.bold),
+                          ),
+                          const SizedBox(height: 30),
+                          Text(
+                            instituteData.address,
+                            textAlign: TextAlign.center,
+                          )
+                        ]),
+                      )
+                    ];
+                  },
+                  body: DefaultTabController(
+                    length: 3,
+                    child: Column(
+                      children: [
+                        const TabBar(
+                          tabs: [
+                            Tab(text: 'Assessment'),
+                            Tab(text: 'Courses'),
+                            Tab(text: 'Batches'),
+                          ],
+                        ),
+                        Expanded(
+                          child: TabBarView(
+                            children: [
+                              AssessmentTab(instituteData.publicAssessmentRefs),
+                              const UpcomingFeature(featureName: 'Courses'),
+                              const UpcomingFeature(featureName: 'Batches'),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
                 );
               }),
         ),
