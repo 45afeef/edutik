8618933ba5655e1c1474261e4cb428ba4cc1262d
Afeef K K <45afeef@gmail.com>
2025-02-16 08:09:19 +0530

    refactor: remove unused BatchRequestStatusConverter class from batch_request_model.dart

--- lib/src/features/courses/da/models/batch_request_model.dart
+++ lib/src/features/courses/da/models/batch_request_model.dart
@@ -1,6 +1,5 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
 
-import '../../../homepage/p/profile/da/user_profile_model.dart';
 import '../../do/entities/batch_request.dart';
 
 part 'batch_request_model.freezed.dart';
@@ -18,29 +17,4 @@ class BatchRequestModel extends BatchRequestEntity with _$BatchRequestModel {
 
   factory BatchRequestModel.fromJson(Map<String, dynamic> json) =>
       _$BatchRequestModelFromJson(json);
-}
-
-// Add a custom converter for BatchRequestStatus
-class BatchRequestStatusConverter
-    implements JsonConverter<BatchRequestStatus, String> {
-  const BatchRequestStatusConverter();
-
-  @override
-  BatchRequestStatus fromJson(String json) {
-    switch (json) {
-      case 'pending':
-        return BatchRequestStatus.pending;
-      case 'accepted':
-        return BatchRequestStatus.accepted;
-      case 'rejected':
-        return BatchRequestStatus.rejected;
-      default:
-        throw ArgumentError('Unknown BatchRequestStatus: $json');
-    }
-  }
-
-  @override
-  String toJson(BatchRequestStatus status) {
-    return status.toString().split('.').last;
-  }
 }
